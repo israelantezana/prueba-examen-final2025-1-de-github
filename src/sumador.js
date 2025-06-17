1d1ec5580f890d7f58f8cac40b5317080d0b7460
Israel Antezana Rojas <israelantezana@gmail.com>
2025-06-17 11:22:32 -0400

    Retorna el mismo numero para cadena de 1 numero

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,8 @@
 function sumar(cadena) {
-  return 0;
+  if (cadena === "") {
+    return 0;
+  }
+  return parseInt(cadena);
 }
 
 export default sumar;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,11 @@
 import sumar from "./sumador.js";
 
 describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
+  it("deberia retornar 0 para cadena vacia", () => {
     expect(sumar("")).toEqual(0);
+  });
+
+  it("deberia retornar el numero para cadena de un solo numero", () => {
+    expect(sumar("2")).toEqual(2);
   });
 });

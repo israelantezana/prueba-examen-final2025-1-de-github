7b67ae76088ad4edcf82dca522d7eac33bff9e5c
Israel Antezana Rojas <israelantezana@gmail.com>
2025-06-17 11:17:38 -0400

    Devuelve 0 para una cadena vacia

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,8 +2,6 @@ import sumar from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+    expect(sumar("")).toEqual(0);
   });
 });
-
-
